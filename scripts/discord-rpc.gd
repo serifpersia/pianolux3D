19c3655f729900872c16962981025aabb6e8ffe6
serifpersia <ramiserifpersia@gmail.com>
2024-11-01 21:51:01 +0100

    in process of migration to new pause menu

--- scripts/discord-rpc.gd
+++ scripts/discord-rpc.gd
@@ -5,7 +5,7 @@ func _ready():
 		var discord_rpc = Engine.get_singleton("DiscordRPC")
 		discord_rpc.app_id = 1226128527049494588
 		discord_rpc.details = "PianoLux in 3D"
-		discord_rpc.state = "Making MIDI piano covers with LEDs"
+		discord_rpc.state = "Making piano covers with LEDs"
 		discord_rpc.large_image = "discord-rcp"
 		discord_rpc.start_timestamp = int(Time.get_unix_time_from_system())
 		discord_rpc.refresh()
