231a3aaf0f9d3c8f19e726ef2f13d7b3a1bcc3b6
serifpersia <ramiserifpersia@gmail.com>
2025-05-27 16:30:55 +0200

    removed keybinds that are no longer used

--- scripts/piano_controller.gd
+++ scripts/piano_controller.gd
@@ -134,7 +134,7 @@ func create_hsb_data(action: String, index: int, hsb: Dictionary) -> Dictionary:
 	}
 
 func send_command_update_color(c: Color):
-	var gamma = 2.8
+	var gamma = 2.2
 	
 	var r = gamma_correction(c.r, gamma)
 	var g = gamma_correction(c.g, gamma)
